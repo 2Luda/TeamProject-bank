633a7b36d66a1c18d6aad8382b5a08ab8ee5b454
2h5i <dev.2h5i@gmail.com>
2022-11-25 21:16:11 +0900

    regex 수정.. pattern.matches

--- TeamProject-bank/src/utils/RegEx.java
+++ TeamProject-bank/src/utils/RegEx.java
@@ -1,31 +1,23 @@
 package utils;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegEx {
 
     public boolean checkPasswordRegEx(String password) {
-        return Pattern.matches(password, "\\d{4}");
+        return Pattern.matches("\\d{4}", password);
     }
 
     public boolean checkNameRegEx(String name) {
-        return checkRegEx(name, "^[가-힣]*$") || checkRegEx(name, "^[a-zA-Z]*$");
+        return Pattern.matches("^[가-힣]*$", name ) || Pattern.matches("^[a-zA-Z]*$", name );
     }
 
     public boolean checkNumberRegEx(long number) {
-        return  checkRegEx(String.valueOf(number), "^[0-9]*$");
+        return  Pattern.matches("^[0-9]*$", String.valueOf(number) );
     }
 
     public boolean checkAccountRegEx(String accountNumber) {
-        return checkRegEx(accountNumber, "^\\d{3}-\\d{4}-\\d{4}$");
+        return Pattern.matches("^\\d{3}-\\d{4}-\\d{4}$", accountNumber );
     }
 
-    public boolean checkRegEx(String value, String regEx) {
-        Pattern pattern = Pattern.compile(regEx);
-
-        Matcher matcher = pattern.matcher(value);
-
-        return matcher.find();
-    }
 }
