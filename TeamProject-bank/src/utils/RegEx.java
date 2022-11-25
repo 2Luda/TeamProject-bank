87674e99228204cad4dd1c916bc4ff3c589e5ed2
jihwan <kiang18@naver.com>
2022-11-25 22:04:53 +0900

    new

--- TeamProject-bank/src/utils/RegEx.java
+++ TeamProject-bank/src/utils/RegEx.java
@@ -14,22 +14,15 @@ public class RegEx {
     }
 
     public boolean checkNameRegEx(String name) {
-        return checkRegEx(name, "^[가-힣]*$") || checkRegEx(name, "^[a-zA-Z]*$");
+        return Pattern.matches(name, "^[가-힣]*$") || Pattern.matches(name, "^[a-zA-Z]*$");
     }
 
     public boolean checkNumberRegEx(long number) {
-        return  checkRegEx(String.valueOf(number), "^[0-9]*$");
+        return Pattern.matches(String.valueOf(number), "^[0-9]*$");
     }
 
     public boolean checkAccountRegEx(String accountNumber) {
-        return checkRegEx(accountNumber, "^\\d{3}-\\d{4}-\\d{4}$");
+        return Pattern.matches(accountNumber, "^\\d{3}-\\d{4}-\\d{4}$");
     }
 
-    public boolean checkRegEx(String value, String regEx) {
-        Pattern pattern = Pattern.compile(regEx);
-
-        Matcher matcher = pattern.matcher(value);
-
-        return matcher.find();
-    }
 }
